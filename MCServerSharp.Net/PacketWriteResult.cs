ba319edc38ec4bf5fc24a2d12e6a5ae35aca23e7
Michal Piatkowski <piatkowski2002@gmail.com>
2020-09-17 08:11:44 +0200

    Pulled up PacketWriteResult

---
+++ MCServerSharp.Net/PacketWriteResult.cs
@@ -0,0 +1,17 @@
+﻿
+namespace MCServerSharp.Net
+{
+    public readonly struct PacketWriteResult
+    {
+        public bool Compressed { get; }
+        public int RawLength { get; }
+        public int Length { get; }
+
+        public PacketWriteResult(bool compressed, int rawLength, int length)
+        {
+            Compressed = compressed;
+            Length = length;
+            RawLength = rawLength;
+        }
+    }
+}
