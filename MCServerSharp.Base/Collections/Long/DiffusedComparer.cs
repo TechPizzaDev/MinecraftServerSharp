4e59d6c250c2e9e518b1f618863e3eb2e9f77cc4
TechnologicalPizza <piatkowski2002@gmail.com>
2020-10-10 00:47:04 +0200

    Added DiffusedComparer

---
+++ MCServerSharp.Base/Collections/Long/DiffusedComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MCServerSharp.Collections
+{
+    public class DiffusedComparer<T> : ILongEqualityComparer<T>
+    {
+        public bool Equals([AllowNull] T x, [AllowNull] T y)
+        {
+            return LongEqualityComparer<T>.Default.Equals(x, y);
+        }
+
+        public int GetHashCode([DisallowNull] T obj)
+        {
+            int code = LongEqualityComparer<T>.Default.GetHashCode(obj);
+            return HashCode.Combine(code);
+        }
+
+        public long GetLongHashCode([DisallowNull] T value)
+        {
+            long code = LongEqualityComparer<T>.Default.GetLongHashCode(value);
+            return LongHashCode.Combine(code);
+        }
+    }
+}
