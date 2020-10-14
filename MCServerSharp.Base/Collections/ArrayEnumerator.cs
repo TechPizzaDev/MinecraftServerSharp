b7a4855d4b251c02126369d4fe4bd3056ea29bc0
TechnologicalPizza <piatkowski2002@gmail.com>
2020-10-14 22:04:02 +0200

    Simplified ArrayEnumerator

--- MCServerSharp.Base/Collections/ArrayEnumerator.cs
+++ MCServerSharp.Base/Collections/ArrayEnumerator.cs
@@ -4,48 +4,50 @@ using System.Collections.Generic;
 
 namespace MCServerSharp.Collections
 {
-    public struct ArrayEnumerator<T> : IEnumerator<T>
+    public struct ArrayEnumerator<T> : IEnumerable<T>, IEnumerator<T>
     {
         private readonly T[] _array;
         private int _index;
 
-        public T Current
-        {
-            get
-            {
-                if ((uint)_index >= (uint)_array.Length)
-                    throw new InvalidOperationException();
-                return _array[_index];
-            }
-        }
-
+        public T Current { get; private set; }
         object? IEnumerator.Current => Current;
 
-        public ArrayEnumerator(T[] array)
+        public ArrayEnumerator(T[] array) : this()
         {
             _array = array ?? throw new ArgumentNullException(nameof(array));
-            _index = -1;
         }
 
         public bool MoveNext()
         {
-            int index = _index + 1;
-            if ((uint)index >= (uint)_array.Length)
-            {
-                _index = _array.Length;
+            if ((uint)_index >= (uint)_array.Length)
                 return false;
-            }
-            _index = index;
+
+            Current = _array[_index++];
             return true;
         }
 
         public void Reset()
         {
-            _index = -1;
+            _index = 0;
         }
 
         public void Dispose()
         {
+        }
+
+        public ArrayEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
 
         public static implicit operator ArrayEnumerator<T>(T[] array)
